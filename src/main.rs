50f12699557140eafa316403e3cfd394a2d12e32
Ivan <zaplatarivan@gmail.com>
2025-08-09 22:53:09 +0000

    Improved cli

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,52 @@
-use cgrep::{walk_directory, parse_file, search};
-use tree_sitter::{Parser, Language, Tree};
+use std::{collections::HashMap, env};
+use tree_sitter::{Tree};
 use tree_sitter_rust::language as tree_sitter_rust;
-use std::collections::HashMap;
+use cgrep::{walk_directory, parse_file, search};
 
 fn main() {
-    // Example usage of the API (currently placeholders)
-    println!("Hello, world!");
-    let exts: Vec<String> = vec!{"rs".to_string()};
+    // Get the search pattern from command line args
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        eprintln!("Usage: {} <dir> <search_pattern>", args[0]);
+        std::process::exit(1);
+    }
+
+    let keyword = &args[2];
+
+    let exts: Vec<String> = vec!["rs".to_string()];
+    let directory = &args[1];
     let files = walk_directory(
-        "/workspaces/ubuntu/.devcontainer/cgrep/src",
-        &exts
+        &directory,
+        &exts,
     );
-    println!("{:?}", files); 
+
+        println!("╔═════════════════════════════════════════════════════════╗");
+        println!("║                      Searching Paths                    ║");
+        println!("╚═════════════════════════════════════════════════════════╝");
+    for file in &files {
+        println!("  • {}", file);
+    }
     let mut trees: HashMap<String, Tree> = HashMap::new();
-    
     for file in &files {
         let tree = parse_file(file);
         trees.insert(file.clone(), tree);
     }
-    let keyword = "let mut cursor = QueryCursor::new();"; // Replace with actual query
+
     let language = tree_sitter_rust();
     let results = search(trees, keyword, language);
-    for result in results {
-        println!("{:?}", result);
+    
+    if results.is_empty() {
+        println!(
+            "❌ Pattern [{}] not found in directory: {}",
+            keyword, directory
+        );
+    } else {
+        println!("╔═════════════════════════════════════════════════════════╗");
+        println!("║        ✅ Found pattern in the following locations      ║");
+        println!("╚═════════════════════════════════════════════════════════╝");
+        for result in &results {
+            println!("  - {}", result);
+        }
     }
 }
